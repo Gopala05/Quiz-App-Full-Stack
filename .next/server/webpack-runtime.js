90f40d5c0798a0c648a5467fea888e0634acaeed
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-06-15 20:31:49 +0530

    The completed Login page.

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("7b1505fae7ad7996")
+/******/ 		__webpack_require__.h = () => ("5fd524adfe12239c")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -20,7 +20,7 @@ const page = () => {
         <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
           <div className="w-3/5 p-5">
             <div className="text-left font-bold text-black">
-              <span className="text-green-500"> Company</span> Name
+              <span className="text-green-500"> Double</span>Trouble
             </div>
             <div className="py-10">
               <h2 className="text-green-500 text-3xl font-bold mb-2">
@@ -48,8 +48,8 @@ const page = () => {
                 </a>
               </div>
               <p className="text-gray-400 my-3">or use your email account</p>
-              <div className="flex flex-col items-center">
-                <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+              <div className="flex flex-col items-center ">
+                <div className="bg-gray-100 w-64 p-2 flex items-center mb-3 rounded">
                   <FaRegEnvelope className="text-gray-400 m-2 pr-600 flex-none " />
                   <input
                     type="email"
@@ -58,7 +58,7 @@ const page = () => {
                     className="bg-gray-100 outline-none text-sm text-black flex-1"
                   />
                 </div>
-                <div className="bg-gray-100 w-64 p-2 flex items-center mb-3">
+                <div className="bg-gray-100 w-64 p-2 flex items-center mb-3 rounded">
                   <MdLockOutline className="text-gray-400 m-2 pr-600 flex-none mb-3" />
                   <input
                     type="password"
@@ -67,6 +67,16 @@ const page = () => {
                     className="bg-gray-100 outline-none text-sm text-black flex-1"
                   />
                 </div>
+                <div className="flex justify-between w-64 mb-5">
+                  <label className="flex items-center text-xs text-black"><input type="checkbox" name="remember" className= "mr-1" />Remember me</label>
+                  <a href="" className="text-xs text-black">Forgot Password?</a>
+                </div>
+                <a
+              href=""
+              className="border-2 border-green-500 text-green-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-green-500 hover:text-white"
+            >
+              Sign In
+            </a>
               </div>
             </div>
           </div>
@@ -75,7 +85,7 @@ const page = () => {
             <h2 className="text-3xl font-bold mb-2">Hello, Friend!</h2>
             <div className="border-2 w-10 border-white inline-block mb-2"></div>
             <p className="mb-10">
-              Fill up personal information and start your journey with us.
+              Fill up some information and start your journey with us.
             </p>
             <a
               href=""
