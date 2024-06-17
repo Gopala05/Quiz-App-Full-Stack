6c7516791026944e480c3b3a14e80e0aa8a143d4
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-06-17 13:27:01 +0530

    Fix errors hindering makemigrations
    Project Urls had a typo where the 'a' in Quizapp was'nt capitals and in models of the application, the 'f' in CharField was'nt capital.

--- mysite/QuizApp/models.py
+++ mysite/QuizApp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class UserDetails(models.Model):
-    username = models.Charfield(max_length=10)
-    password = models.Charfield(max_length=10)
+    username = models.CharField(max_length=10)
+    password = models.CharField(max_length=10)
 
 
     def __string__(self):
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,5 +17,5 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include("Quizapp.urls")),
+    path('', include("QuizApp.urls")),
 ]
