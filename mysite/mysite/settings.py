307a2880ab0b40c1679bd0a0f4e1562711acfed4
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-06-17 13:45:10 +0530

    Code post migrations and changing default database to postgresql.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,10 +77,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
