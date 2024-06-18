21a7586a8703eb9d890d71db9c0bc0eb99478b05
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-06-18 23:54:35 +0530

    Code defining schema required to store questions and related attributes.

--- mysite/QuizApp/models.py
+++ mysite/QuizApp/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 
 class UserDetails(models.Model):
-    username = models.CharField(max_length=10)
-    password = models.CharField(max_length=10)
+    username = models.CharField(max_length=10, null = False, blank = False, unique=True)
+    password = models.CharField(max_length=10, null = False, blank = False)
 
     def __string__(self):
         return self.username
 
+class Quiz(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
     
