9cfdd6dbb795a24a4e2b7ed4e9947b6ef2bb2660
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-06-19 20:02:49 +0530

    Update the defined models
    Create a clean schema for ease of use.

--- mysite/QuizApp/models.py
+++ mysite/QuizApp/models.py
@@ -1,31 +1,32 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class UserDetails(models.Model):
-    username = models.CharField(max_length=10, null = False, blank = False, unique=True)
+    username = models.CharField(primary_key=True, max_length=20)
     password = models.CharField(max_length=10, null = False, blank = False)
+    name = models.CharField(max_length=10, null = False, blank = False)
 
     def __string__(self):
         return self.username
+    
 
-class Quiz(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
+class MakeQuiz(models.Model):
+    title = models.CharField(unique=True,blank=False, null=False, max_length=20)
+    no_of_questions = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    marks_per_question = models.PositiveIntegerField(validators=[MinValueValidator(1)]) 
+    no_of_choices = models.PositiveIntegerField(validators=[MinValueValidator(2)]) 
+    question_number = models.PositiveIntegerField(primary_key=True, validators=[MinValueValidator(1)])
+    question = models.CharField(max_length=100, unique=True, blank=False, null= False)
 
-    def __str__(self):
-        return self.title
 
-class Question(models.Model):
-    quiz = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
-    text = models.CharField(max_length=500)
+class TakeQuiz(models.Model):
+    question_number = models.ForeignKey(MakeQuiz, unique=True)
+    selected_choice = models.IntegerField()
+    marks_per_question = models.ForeignKey(MakeQuiz)
+    marks_alloted = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.text
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
-    text = models.CharField(max_length=200)
-    is_correct = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.text
+class UserScore(models.Model):
+    quiz_title = models.ForeignKey(MakeQuiz, on_delete=models.CASCADE)
+    marks_scored = models.IntegerField()
+    username = models.ForeignKey(UserDetails, unique=True)
     
