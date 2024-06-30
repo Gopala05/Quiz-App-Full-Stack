2f34654f4760d2a83c6ddb13d75ebb92a3e79fb4
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-06-30 21:22:54 +0530

    Models Update

--- backend/QuizApp/models.py
+++ backend/QuizApp/models.py
@@ -12,32 +12,33 @@ class UserDetails(models.Model):
 class MakeQuiz(models.Model):
     title = models.CharField(primary_key=True, max_length=20, unique=True, blank=False, null=False)
     no_of_questions = models.PositiveIntegerField(validators=[MinValueValidator(1)])
-    marks_per_question = models.PositiveIntegerField(validators=[MinValueValidator(1)], default=1) 
-    no_of_choices = models.PositiveIntegerField(validators=[MinValueValidator(2)]) 
-    options = models.CharField(blank=False, null=False)
-    questions = models.CharField(unique=True, blank=False, null=False)
+    marks_per_question = models.PositiveIntegerField(validators=[MinValueValidator(1)], default=1)
+    no_of_choices = models.PositiveIntegerField(validators=[MinValueValidator(2)])
+    options = models.JSONField(blank=False, null=False)
+    questions = models.JSONField(unique=True, blank=False, null=False)
     timer_seconds = models.IntegerField(null=True, blank=True, default=10)
-    correct_option = models.CharField()
-
+    correct_option = models.JSONField(blank=False, null=False)
+    
     def __str__(self):
         return self.title
 
-class TakeQuiz(models.Model):
-    quiz = models.ForeignKey(MakeQuiz, on_delete=models.CASCADE)
-    question_number = models.PositiveIntegerField()
-    selected_choice = models.IntegerField()
-    marks_per_question = models.PositiveIntegerField(default=2)
-    marks_alloted = models.BooleanField(default=False)
+# class TakeQuiz(models.Model):
+#     quiz = models.ForeignKey(MakeQuiz, on_delete=models.CASCADE)
+#     question_number = models.PositiveIntegerField()
+#     selected_choice = models.IntegerField()
+#     marks_per_question = models.PositiveIntegerField(default=2)
+#     marks_alloted = models.BooleanField(default=False)
 
-    class Meta:
-        unique_together = ('quiz', 'question_number')
+#     class Meta:
+#         unique_together = ('quiz', 'question_number')
 
-    def __str__(self):
-        return f"Quiz: {self.quiz.title}, Question: {self.question_number}"
+#     def __str__(self):
+#         return f"Quiz: {self.quiz.title}, Question: {self.question_number}"
 
 class UserScore(models.Model):
     quiz_title = models.ForeignKey(MakeQuiz, on_delete=models.CASCADE)
     marks_scored = models.IntegerField()
+    maximum_score = models.IntegerField()
     username = models.ForeignKey(UserDetails, on_delete=models.CASCADE)
 
     class Meta:
