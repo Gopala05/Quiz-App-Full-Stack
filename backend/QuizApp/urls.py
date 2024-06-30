8c91e1138e1b97c116e3b4cf5e482ff5d0ffb5e0
Gopala05 <gopigopala05vkbrg@gmail.com>
2024-06-30 21:23:38 +0530

    Routes Update

--- backend/QuizApp/urls.py
+++ backend/QuizApp/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('signup/', SignUp.as_view(), name = 'signup'),
-    path('signin/', SignIn.as_view(), name = 'signin'),
-    path('create/', CreateQuiz.as_view(), name = 'takeInput'),
+    path('signup', SignUp.as_view(), name = 'signup'),
+    path('signin', SignIn.as_view(), name = 'signin'),
+    path('create', CreateQuiz.as_view(), name = 'create_quiz'),
+    path('quiz', GetQuiz.as_view(), name = 'get_quiz'),
+    path('score', Score.as_view(), name = 'score'),
+    path('results', GetResults.as_view(), name = 'results'),
 ]
